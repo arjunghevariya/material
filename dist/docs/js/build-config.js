6dfe18a9123d13c77cc90d5719f936bc173a910d
arjun <arjun.ghevariya@infizer.com>
2017-02-28 14:14:07 +0530

    add dist folder

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+angular.module('docsApp').constant('BUILDCONFIG', {
+  "ngVersion": "1.5.5",
+  "version": "1.1.3",
+  "repository": "https://github.com/angular/material",
+  "commit": "d3938fcb1aa9e8911f99968c813d4e6559c39db1",
+  "date": "2017-02-27 16:19:17 +0530"
+});
